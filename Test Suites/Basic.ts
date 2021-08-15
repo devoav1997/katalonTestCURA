7cd8bac8e6824263d4444e85a1078985328845aa
devoA <devo.avidianto@batmandiri.com>
2021-08-15 14:43:32 +0700

    test suites and report

---
+++ Test Suites/Basic.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Basic test suite for CURA</description>
+   <name>Basic</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a9fac439-e1fc-47d8-9c44-964be8b18a5c</testSuiteGuid>
+   <testCaseLink>
+      <guid>99c17edb-9e08-4045-b125-0aa81aad4ae2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Basic</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
